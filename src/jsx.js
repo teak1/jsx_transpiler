a5d1dacdaa0882075ef425d0de9f41d85cdb9995
teak1 <trainian@gmail.com>
2019-02-04 22:02:44 -0800

    Delete jsx.js

--- src/jsx.js
+++
@@ -1,8 +0,0 @@
-function _JSX(parent, thing) {
-    if(thing.constructor==Array)return thing.forEach(itm=>_JSX(parent,itm));
-    if (thing.nodeType || thing.textContent) {
-        parent.appendChild(thing);
-    } else {
-        parent.appendChild(document.createTextNode(thing.toString()));
-    }
-}
